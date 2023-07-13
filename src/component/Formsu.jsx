d97c21cfa2488e8a249eac9d0442701a1a278b3b
Techharik <khariprasath30@gmail.com>
2023-07-13 23:34:51 +0530

    Ui is SET

---
+++ src/component/Formsu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Formsu() {
+  return (
+    <form className='text-center m-4'>
+        <input  type='text' placeholder='Enter Notes' className='border-4 p-2 rounded' />
+        <button className='bg-orange-500 p-2 rounded ml-2'>Click</button>
+    </form>
+  )
+}
+
+export default Formsu
